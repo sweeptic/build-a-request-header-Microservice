5d0892e67835ae897df1d7aba3fc986772c6ae44
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-27 11:18:45 +0200

    remove local serve host

--- controllers/api.js
+++ controllers/api.js
@@ -6,13 +6,7 @@ exports.getHello = (req, res, next) => {
 
 exports.getInfo = (req, res, next) => {
   const ip = req.headers['x-forwarded-for'].split(',')[0];
-  const lang = req.headers['accept-language'];
-  const soft = req.headers['user-agent'];
-  res.json({ ipaddress: ip, language: lang, software: soft });
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress: ip, language: language, software: software });
 };
-
-/*
-"ipaddress":"88.132.237.7",
-"language":"en-GB,en-US;q=0.9,en;q=0.8,hu;q=0.7,fr;q=0.6","software":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"
-
-*/
